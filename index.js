5394bbf6e17f434ef06a1d294f293c5fada3e832
ManhDevFull <thanhmanh181125@gmail.com>
2024-07-10 16:56:20 +0700

    Set Upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const flash = require('express-flash')
@@ -23,6 +24,10 @@ app.set("view engine", "pug")
 app.use(cookieParser('keyweb cat'))
 app.use(session({cookie: {maxAge: 60000}}))
 app.use(flash())
+// TinyMce
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')))
+// End TinyMce
+
 app.locals.prefixAdmin = systemConfig.prefixAdmin
 app.use(express.static(`${__dirname}/public`))
 
