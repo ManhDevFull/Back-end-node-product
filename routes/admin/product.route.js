c7a8568147863fddbefc5e3a502934c89f2ef64e
ManhDevFull <thanhmanh181125@gmail.com>
2024-07-10 14:59:56 +0700

    Set Upload

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,16 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
 const upload = multer()
 const controllers = require("../../controllers/admin/product.controller")
+const uploadCloud = require('../../middlewares/admin/uploadCloud.middleware')
 
-cloudinary.config({
-    cloud_name: 'dl3a0os7m',
-    api_key: '729718785359536',
-    api_secret: 'uyUDkzACrB_5FQYuYlblKQJ_Ce0'
-})
+
 
 router.get("/", controllers.product)
 router.patch("/change-status/:status/:id", controllers.changeStatus)
@@ -18,32 +13,5 @@ router.patch("/change-multi", controllers.changeMulti)
 router.get("/create", controllers.create)
 
 router.post("/create",
-    upload.single('thumbnail'),
-    function (req, res, next) {
-        if (req.file) {
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                        (error, result) => {
-                            if (result) {
-                                resolve(result)
-                            } else {
-                                reject(error)
-                            }
-                        }
-                    )
-                    streamifier.createReadStream(req.file.buffer).pipe(stream)
-                })
-            }
-            async function upload(req) {
-                let result = await streamUpload(req)
-                req.body[req.file.fieldname] = result.url
-                next()
-            }
-            upload(req)
-        } else {
-            next()
-        }
-    }
-    , controllers.createPost)
+    upload.single('thumbnail'), uploadCloud.upload, controllers.createPost)
 module.exports = router
